e1ac731a07e4dec1043e9b52f961fd84796478fd
shakil_usgaonker <shakil_usgaonker@PSGA0191.persistent.co.in>
2014-08-26 14:52:23 +0530

    Modified existing code to include 	-	Create / Update Queue 	-	Delete Queue

---
+++ src/main/java/com/ibm/softlayer/util/APIConstants.java
@@ -0,0 +1,10 @@
+package com.ibm.softlayer.util;
+
+/**
+ * The Class APIConstants.
+ */
+public class APIConstants {
+
+	/** The Constant QUEUES_API. */
+	public static final String QUEUES_API = "queues";
+}
