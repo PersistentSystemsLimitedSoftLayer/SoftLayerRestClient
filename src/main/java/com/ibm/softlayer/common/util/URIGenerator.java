cc1fc3451d47048c047c95d159dc71b0e73a1bd3
root <root@lksh-compute.persistent.co.in>
2014-09-01 18:13:53 +0530

    Ticketing Service Addition

--- src/main/java/com/ibm/softlayer/common/util/URIGenerator.java
+++ src/main/java/com/ibm/softlayer/common/util/URIGenerator.java
@@ -1,14 +1,25 @@
 package com.ibm.softlayer.common.util;
 
-import com.ibm.softlayer.util.APIConstants;
-
 /**
  * The Class URIGenerator.
  */
 public class URIGenerator {
 
 	/** The Constant properties. */
-	private static final SLProperties properties = SLProperties.getInstance();	
+	private static final SLProperties properties = SLProperties.getInstance();
+	
+	/**
+	 * Gets the public base url.
+	 *
+	 * @return the public base url
+	 */
+	private static StringBuffer getPublicBaseURL () {
+		StringBuffer url = new StringBuffer();
+		url.append("https://");
+		url.append(properties.getProperty(SLProperties.SL_PUBLIC_HOSTNAME)).append("/");
+		url.append(properties.getProperty(SLProperties.SL_VERSION)).append("/");
+		return url;
+	}
 	
 	/**
 	 * Gets the url.
@@ -20,8 +31,7 @@ public class URIGenerator {
 	public static String getURL(String accountId, String api) {
 		
 		//generate the get queues URL
-		StringBuffer url = new StringBuffer();
-		url.append(properties.getProperty(SLProperties.SL_MESSAGING_BASE_API));
+		StringBuffer url = getPublicBaseURL();
 		if(!url.toString().endsWith("/")) {
 			url.append("/");
 		}
@@ -35,9 +45,44 @@ public class URIGenerator {
 		return url.toString();
 	}
 	
+	
+	
+	private static StringBuffer getRestPublicBaseURL () {
+		StringBuffer url = new StringBuffer();
+		url.append("https://");
+		url.append(properties.getProperty(SLProperties.SL_BASE_API)).append("/");
+		//url.append(properties.getProperty(SLProperties.SL_REST_VERSION)).append("/");
+		return url;
+	}
+	
+	/**
+	 * Gets the url.
+	 *
+	 * @param accountId the account id
+	 * @param api the api
+	 * @return the url
+	 */
+	public static String getRestURL(String api) {
+		
+		//generate the get queues URL
+		StringBuffer url = getRestPublicBaseURL();
+		if(!url.toString().endsWith("/")) {
+			url.append("/");
+		}
+		
+		//append the account id
+		//url.append(accountId).append("/");
+		
+		//append the auth to the URL
+		url.append(api);
+		
+		return url.toString();
+	}
+	
 	/**
 	 * Gets the SL api url.
 	 *
+	 * @param appendAPI the append api
 	 * @return the SL api url
 	 */
 	public static String getVirtualGuestAPIURL(){
@@ -51,22 +96,17 @@ public class URIGenerator {
 	}
 	
 	
+	
 	/**
-	 * Gets the SL messaging apiurl.
+	 * Gets the SL api url.
 	 *
-	 * @return the SL messaging apiurl
+	 * @param appendAPI the append api
+	 * @return the SL api url
 	 */
-	public static String getSLMessagingAPIURL(){
-		StringBuffer url = new StringBuffer();
-		url.append(properties.getProperty(SLProperties.SL_MESSAGING_BASE_API));
-		if(!url.toString().endsWith("/")) {
-			url.append("/");
-		}
-		
-		//append the account id
-		url.append(properties.getProperty(SLProperties.SL_MESSAGING_ACCOUNTID)).append("/");
-				
-		url.append(APIConstants.QUEUES_API);
-		return url.toString();
-	}		
+
+	
+	
+	
+	
+	
 }
