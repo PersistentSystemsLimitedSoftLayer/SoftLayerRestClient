0d5586cebe499c61a15708f52f4ca91af5b88055
shakil_usgaonker <shakil_usgaonker@PSGA0191.persistent.co.in>
2014-09-03 10:47:51 +0530

    Implemented following functioanlities 	-	Get Ticket Status 	-	Get Assigned User for a Ticket 	-	Get Ticket Updates 	-	Get Open Tickets 	-	Get Closed Tickets

--- src/main/java/com/ibm/softlayer/ticket/service/GetTicketsService.java
+++ src/main/java/com/ibm/softlayer/ticket/service/GetTicketsService.java
@@ -1,32 +1,16 @@
 package com.ibm.softlayer.ticket.service;
 
-import org.apache.wink.client.ClientResponse;
+import java.util.List;
+
 import org.apache.wink.json4j.JSONArray;
 
-import com.ibm.softlayer.common.client.SoftLayerServiceClient;
-import com.ibm.softlayer.common.service.AbstractService;
-import com.ibm.softlayer.common.util.URIGenerator;
 import com.ibm.softlayer.util.APIConstants;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * The Class GetTicketsService.
  */
-public class GetTicketsService extends AbstractService {
+public class GetTicketsService extends AbstractTicketsService {
 	
-	/** The Constant logger. */
-	private static final Logger logger = LoggerFactory.getLogger(GetTicketsService.class);	
-	
-	/** The Constant GET_OPEN_TICKETS. */
-	private static final String GET_OPEN_TICKETS = "getOpenTickets";
-	
-	/** The Constant GET_CLOSED_TICKETS. */
-	private static final String GET_CLOSED_TICKETS = "getClosedTickets";
-	
-	/** The Constant GET_TICKETS. */
-	private static final String GET_TICKETS = "getTickets";
 	
 	/**
 	 * Instantiates a new gets the tickets service.
@@ -36,29 +20,7 @@ public class GetTicketsService extends AbstractService {
 	 */
 	public GetTicketsService(String username, String apikey) {
 		super(username, apikey);	
-	}
-	
-	/**
-	 * Gets the open tickets.
-	 *
-	 * @return the open tickets
-	 * @throws Exception the exception
-	 */
-	public JSONArray getOpenTickets()throws Exception 
-	{			
-		return getTickets(GET_OPEN_TICKETS);
-	}
-	
-	/**
-	 * Gets the closed tickets.
-	 *
-	 * @return the closed tickets
-	 * @throws Exception the exception
-	 */
-	public JSONArray getClosedTickets()throws Exception 
-	{
-		return getTickets(GET_CLOSED_TICKETS);		
-	}
+	}		
 	
 	/**
 	 * Gets the all tickets.
@@ -66,41 +28,19 @@ public class GetTicketsService extends AbstractService {
 	 * @return the all tickets
 	 * @throws Exception the exception
 	 */
-	public JSONArray getAllTickets()throws Exception 
-	{		
-		return getTickets(GET_TICKETS);
-	}
-	
+	public JSONArray getAllTickets()throws Exception {		
+		return getTickets(APIConstants.GET_TICKETS, null);
+	}		
+
 	/**
-	 * Gets the tickets.
+	 * Gets the all tickets.
 	 *
-	 * @param ticketsURL the tickets url
-	 * @return the tickets
+	 * @param objectMasks the object masks
+	 * @return the all tickets
 	 * @throws Exception the exception
 	 */
-	private JSONArray getTickets(String ticketsURL) throws Exception 
-	{
-		logger.info("Executing "+ ticketsURL +" getAllTickets for username: " + getUsername());
-		
-		//generate the get queues URL	
-		StringBuffer url = new StringBuffer(URIGenerator.getSLBaseURL(APIConstants.ACCOUNT_ROOT_API));
-		if(!url.toString().endsWith("/")) {
-			url.append("/");
-		}
-		url.append(ticketsURL);
-		
-		//execute the get tickets call
-		SoftLayerServiceClient client = new SoftLayerServiceClient();		
-		ClientResponse clientResponse = client.executeGET(url.toString(), null, getCredentialsColonSeperated());
-		String response = clientResponse.getEntity(String.class);
-		logger.info("Executed get tickets for username: " + getUsername() + ", clientResponse: " + clientResponse.getStatusCode() + ", response: " + response);
-		
-		if(clientResponse.getStatusCode() == 200){
-			JSONArray json = new JSONArray(response);
-			logger.debug("GetTickets JSON Response: " + response);
-			return json;			
-		}
-		
-		throw new Exception("Could not retrieve the token: Code: " + clientResponse.getStatusCode() + ", Reason: " + response);			
-	}
+	public JSONArray getAllTickets(List<String> objectMasks)throws Exception {		
+		return getTickets(APIConstants.GET_TICKETS, objectMasks);
+	}		
+
 }
