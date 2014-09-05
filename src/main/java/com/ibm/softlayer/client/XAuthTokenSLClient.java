62747d49927d6b4a00d72a863b017d9afa53b4d7
shakil_usgaonker <shakil_usgaonker@PSGA0191.persistent.co.in>
2014-09-05 13:01:33 +0530

    Implemented Following functionalities 	-	Upload File (zip) 	-	Get File Details 	-	Delete File Object

--- src/main/java/com/ibm/softlayer/client/XAuthTokenSLClient.java
+++ src/main/java/com/ibm/softlayer/client/XAuthTokenSLClient.java
@@ -1,10 +1,28 @@
 package com.ibm.softlayer.client;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.apache.wink.client.ClientResponse;
+import org.apache.wink.client.Resource;
+import org.apache.wink.client.RestClient;
+import org.apache.wink.common.internal.MultivaluedMapImpl;
+import org.apache.wink.common.model.multipart.BufferedOutMultiPart;
+import org.apache.wink.common.model.multipart.OutPart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * The Class XAuthTokenSLClient.
  */
 public class XAuthTokenSLClient extends AbstractSoftLayerClient {	
+	
+	/** The Constant logger. */
+	private static final Logger logger = LoggerFactory.getLogger(XAuthTokenSLClient.class);
 	
 	/**
 	 * Instantiates a new x auth token sl client.
@@ -14,5 +32,51 @@ public class XAuthTokenSLClient extends AbstractSoftLayerClient {
 	public XAuthTokenSLClient(String token) {			
 		setxAuthToken(token);
 		setUseAuthToken(true);
-	}	
+	}
+	
+	/**
+	 * Upload file.
+	 *
+	 * @param url the url
+	 * @param inputFile the input file
+	 * @return the client response
+	 * @throws Exception the exception
+	 */
+	public ClientResponse uploadFile(String url, File inputFile) throws Exception {
+		
+		logger.info("Executing uploadFile:PUT for following URL: " + url);
+		byte[] bytes = new byte[(int) inputFile.length()];
+		FileInputStream fis = new FileInputStream(inputFile);
+		fis.read(bytes);
+		fis.close();
+		
+		//append the filename to the url
+		if(!url.endsWith("/")) {
+			url += "/";
+		}
+		url += inputFile.getName();
+		
+		// Create a multipart request
+		BufferedOutMultiPart mp = new BufferedOutMultiPart();
+		
+		// Add file part
+		OutPart fp = new OutPart();
+		MultivaluedMap<String,String> headers = new MultivaluedMapImpl<String,String>();
+		headers.add("name", "file_part");
+		headers.add("filename", inputFile.getName());
+		fp.setHeaders(headers);
+		
+		fp.setContentType(MediaType.TEXT_PLAIN);
+		fp.setBody(inputFile);     
+		mp.addPart(fp);
+		
+		RestClient client = new RestClient(getClientConfig());		
+		Resource resource = client.resource(url);			
+		resource.header("X-Auth-Token", getxAuthToken());
+		
+		ClientResponse clientResponse = resource.put(mp);
+		String response = clientResponse.getEntity(String.class);		
+		logger.info("Executed uploadFile:PUT for following URL: " + url + ", clientResponse: " + clientResponse.getStatusCode() + ", response: " + response);
+		return clientResponse;
+	}
 }
---
+++ src/test/java/com/ibm/softlayer/storage/service/FileObjectServiceTest.java
@@ -0,0 +1,68 @@
+package com.ibm.softlayer.storage.service;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+import org.apache.wink.json4j.JSONObject;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.ibm.softlayer.storage.object.service.ContainerService;
+import com.ibm.softlayer.storage.object.service.FileObjectService;
+import com.ibm.softlayer.util.APIConstants;
+import com.ibm.softlayer.util.TokenGenerator;
+import com.ibm.softlayer.util.UnitTestConstants;
+
+public class FileObjectServiceTest {
+
+	private static String authToken = null;
+	private static String storageURL = null;
+	private static String containerName = "container12";
+	private static String uploadFileName = "logs.zip";
+	
+	@BeforeClass
+	public static void authenticateUser() throws Exception {
+		JSONObject authTokens = TokenGenerator.getTokenForStorage(UnitTestConstants.STORAGE_TENANT_NAME, UnitTestConstants.SL_USERNAME, UnitTestConstants.SL_APIKEY);
+		assertNotNull(authTokens);
+		assertNotNull(authTokens.get(APIConstants.X_AUTH_TOKEN));
+		assertNotNull(authTokens.get(APIConstants.X_STORAGE_URL));
+		authToken = authTokens.getString(APIConstants.X_AUTH_TOKEN);
+		storageURL = authTokens.getString(APIConstants.X_STORAGE_URL);
+	}
+	
+	@Test
+	public void testCreateContainer() throws Exception {
+		ContainerService service = new ContainerService(storageURL, authToken);
+		boolean created = service.createContainer(containerName);
+		assertEquals(true, created);
+	}
+	
+	@Test
+	public void testUploadFileToContainer() throws Exception {
+		String fileLocation = Thread.currentThread().getContextClassLoader().getResource("logs.zip").getFile();
+		FileObjectService service = new FileObjectService(storageURL, authToken, containerName);
+		boolean uploaded = service.uploadFile(fileLocation);
+		assertEquals(true, uploaded);
+	}
+	
+	@Test
+	public void testGetUploadFileFromContainer() throws Exception {
+		FileObjectService service = new FileObjectService(storageURL, authToken, containerName);
+		boolean retrieved = service.getFileObject(uploadFileName);
+		assertEquals(true, retrieved);
+	}
+	
+	@Test
+	public void testDeleteUploadFileFromContainer() throws Exception {
+		FileObjectService service = new FileObjectService(storageURL, authToken, containerName);
+		boolean retrieved = service.deleteFileObject(uploadFileName);
+		assertEquals(true, retrieved);
+	}
+	
+	@Test
+	public void testDeleteContainer() throws Exception {
+		ContainerService service = new ContainerService(storageURL, authToken);
+		boolean deleted = service.deleteContainer(containerName);
+		assertEquals(true, deleted);
+	}	
+}
