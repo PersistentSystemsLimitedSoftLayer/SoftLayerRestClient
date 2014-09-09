f31593a0c0e8a60f376027f8f97694b8262d3cdc
mandarparadkar <mandar_paradkar@persistent.co.in>
2014-09-09 11:02:57 +0530

    Added Update Ticket Service Commented the failing test cases in File Object Service.

---
+++ src/main/java/com/ibm/softlayer/ticket/service/UpdateTicketService.java
@@ -0,0 +1,90 @@
+package com.ibm.softlayer.ticket.service;
+
+import org.apache.wink.client.ClientResponse;
+import org.apache.wink.json4j.JSONArray;
+import org.apache.wink.json4j.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ibm.softlayer.client.BasicAuthorizationSLClient;
+import com.ibm.softlayer.util.APIConstants;
+import com.ibm.softlayer.util.URIGenerator;
+import com.ibm.softlayer.util.UnitTestConstants;
+
+public class UpdateTicketService extends AbstractGetTicketsService {
+
+	/** The Constant logger. */
+	private static final Logger logger = LoggerFactory.getLogger(ListSubjectsService.class);
+
+	
+	public UpdateTicketService(String username, String apikey) {
+		super(username, apikey);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 * @param assinedToUser
+	 * @param SubjectId
+	 * @param tittle
+	 * @param content
+	 * @param ticketId
+	 * @return 
+	 * @throws Exception
+	 */
+	public JSONObject updateTicket (String assinedToUser, String SubjectId,String tittle,String content, String ticketId) throws Exception{
+		
+		GetTicketByIdService service = new GetTicketByIdService(UnitTestConstants.SL_USERNAME, UnitTestConstants.SL_APIKEY);
+		JSONObject jsonObject = service.getTicketById(ticketId);
+		
+		/**
+		 * removes earlier values in ticket
+		 */
+		jsonObject.remove(SubjectId);
+		jsonObject.remove(assinedToUser);
+		jsonObject.remove(tittle);
+	
+		/**
+		 * adds new values to ticket
+		 */
+		
+		jsonObject.put("subjectId", SubjectId);
+		jsonObject.put("assignedUserId", assinedToUser);
+		jsonObject.put("title", tittle);
+		
+		JSONArray parameter = new JSONArray();
+		parameter.add(jsonObject);
+		parameter.add(content);
+		
+		JSONObject json = new JSONObject();
+		json.put("parameters", parameter);
+		
+		
+        logger.info("Executing update ticket username: " + UnitTestConstants.SL_USERNAME);
+		
+		StringBuffer url = new StringBuffer(URIGenerator.getSLBaseURL(APIConstants.TICKETS_ROOT_API));
+		if(!url.toString().endsWith("/")) {
+			url.append("/");
+		}
+		url.append(ticketId);
+		url.append("/edit.json");
+		
+		BasicAuthorizationSLClient client = new BasicAuthorizationSLClient(UnitTestConstants.SL_USERNAME, UnitTestConstants.SL_APIKEY);		
+		ClientResponse clientResponse = client.executePOST(url.toString(), json.toString());
+		String response = clientResponse.getEntity(String.class);
+		logger.info("Executed update Ticket: clientResponse: " + clientResponse.getStatusCode() + ", response: " + response);
+		
+		if(clientResponse.getStatusCode() == 200){
+			JSONObject json1 = new JSONObject(response);
+			logger.debug("update Ticket: JSON Response: " + response);
+			return json1;
+			
+		}
+		
+		throw new Exception("Could not update Ticket: Code: " + clientResponse.getStatusCode() + ", Reason: " + response);		
+				
+	}
+	
+
+	
+}
